59431a057acd1c71410b74b171c2da6543e2c29e
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2024-01-18 21:07:22 -0500

    remove blank lines from input

--- replace-strings.sh
+++ replace-strings.sh
@@ -2,4 +2,4 @@
 while IFS="|" read -r find replace glob
 do
     find $STAGING/$glob -type f -exec sed -i "s|$find|$replace|g" {} +
-done <<< "$INPUTS_REPLACE"
+done <<< "$(grep '\S' <<< $INPUTS_REPLACE)"
--- stage-files.sh
+++ stage-files.sh
@@ -3,4 +3,4 @@ mkdir $STAGING
 while IFS="|" read -r source destination
 do
     rsync -a --exclude=".git" $REPO/$source $STAGING/$destination
-done <<< "$INPUTS_COPY"
+done <<< "$(grep '\S' <<< $INPUTS_COPY)"
