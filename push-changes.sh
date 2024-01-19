af63e2fb17592888d346caf0e1d06208586110f2
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2024-01-18 20:26:08 -0500

    ensure git pull before push

--- push-changes.sh
+++ push-changes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd $REPO
+git pull --all
 git config user.name "$(git show -s --format='%an' HEAD)"
 git config user.email "$(git show -s --format='%ae' HEAD)"
 git checkout $BRANCH -- || git switch --orphan $BRANCH
