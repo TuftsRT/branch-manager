04f1a9d7e101c2d7e422fef15badde89ca0d92f6
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2024-01-26 14:58:08 -0500

    handle all inputs as strings per github spec

--- push-changes.sh
+++ push-changes.sh
@@ -4,7 +4,7 @@ git pull --all
 git config user.name "$(git show -s --format='%an' HEAD)"
 git config user.email "$(git show -s --format='%ae' HEAD)"
 git checkout $BRANCH -- || git switch --orphan $BRANCH
-if [ $CLEAR = true ]; then git rm -rfq *; fi
+if [ "$CLEAR" == "true" ]; then git rm -rfq *; fi
 rsync -a --exclude=".git" ../$STAGING/ ./
 git add -A
 git commit -m "$MESSAGE"
