6810967479179a834d82b6c1b9de8c6c1049e9ec
Uku-Kaspar Uustalu <ukukaspar.uustalu@gmail.com>
2024-09-16 12:04:48 -0400

    pull before attempting branch switch

--- push-changes.sh
+++ push-changes.sh
@@ -4,6 +4,7 @@ cd $REPO
 git reset --hard
 git config user.name "$(git show -s --format='%an' HEAD)"
 git config user.email "$(git show -s --format='%ae' HEAD)"
+git pull --all
 git checkout $BRANCH -- || git switch --orphan $BRANCH
 if [ "$CLEAR" == "true" ]; then git rm -rfq *; fi
 rsync -a --exclude=".git" ../$STAGING/ ./
